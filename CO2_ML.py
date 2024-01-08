c352bc3cb65b3bca4a27ffad69b002ef318bcc58
HakanGnes <100923570+HakanGnes@users.noreply.github.com>
2024-01-08 15:47:30 +0300

    Create CO2_ML.py

---
+++ CO2_ML.py
@@ -0,0 +1,265 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+from pandas.core.common import SettingWithCopyWarning
+from sklearn.exceptions import ConvergenceWarning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=ConvergenceWarning)
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+from pandas.core.common import SettingWithCopyWarning
+from sklearn.exceptions import ConvergenceWarning
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=ConvergenceWarning)
+warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
+
+dff = pd.read_csv("Data/CO2_emission.csv")
+df = dff.copy()
+df.head()
+
+def check_df(dataframe):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(3))
+    print("##################### Tail #####################")
+    print(dataframe.tail(3))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+#############################################
+# 1. Separation of variables according to type
+#############################################
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optional
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+
+    """
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+cat_cols
+num_cols
+df["Make"].info
+df = df.drop("Model_Year",axis=1)
+#############################################
+# 2. Analysis of Categorical Variables
+#############################################
+
+def cat_summary(dataframe, col_name, plot=False):
+
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("##########################################")
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show(block=True)
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)
+
+
+#############################################
+# 3. Analysis of Numerical Variables
+#############################################
+
+def num_summary(dataframe, numerical_col, plot=False):
+
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=20)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show(block=True)
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+#############################################
+# 4. Analysis of Target Variable
+#############################################
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}), end="\n\n\n")
+    print("######################")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "CO2_Emissions", col)
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}), end="\n\n\n")
+
+for col in num_cols:
+    target_summary_with_num(df, "CO2_Emissions", col)
+
+
+#############################################
+# 5. Analysis of Correlation
+#############################################
+f, ax = plt.subplots(figsize=[18, 13])
+sns.heatmap(df[num_cols].corr(), annot=True, fmt=".2f", ax=ax, cmap="RdBu")
+ax.set_title("Correlation Matrix", fontsize=20)
+plt.show(block=True)
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show(block=True)
+    return drop_list
+
+high_correlated_cols(df, plot=True)
+
+#############################################
+# Outliers
+#############################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def replace_with_thresholds(dataframe, variable, q1=0.05, q3=0.95):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable, q1=0.05, q3=0.95)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+df.head()
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+df.loc[:,'Make']=df.loc[:,'Make'].astype('category').cat.codes
+df.loc[:,'Model']=df.loc[:,'Model'].astype('category').cat.codes
+df.loc[:,'Vehicle_Class']=df.loc[:,'Vehicle_Class'].astype('category').cat.codes
+df.loc[:,'Transmission']=df.loc[:,'Transmission'].astype('category').cat.codes
+
+df.head()
+
+df.columns
+
+y = df["CO2_Emissions"]
+X = df.drop(["CO2_Emissions","Fuel_Consumption_comb(L/100km)",'Fuel_Consumption_in_City(L/100 km)',"Cylinders"], axis=1)
+from sklearn.model_selection import train_test_split,GridSearchCV
+from lazypredict.Supervised import LazyClassifier
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=42)
+
+
+lcf = LazyClassifier(predictions = True)
+models, predictions = lcf.fit(X_train, X_test, y_train, y_test)
+models
+
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.tree import DecisionTreeRegressor
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=42)
+#Parameters for grid search
+param_grid={'criterion':['squared_error','friedman_mse','poisson'],
+    'max_depth':[3,5,7,None],
+    'max_features':[2,3,5,None]
+}
+#Training model with grid search
+cv=GridSearchCV(DecisionTreeRegressor(),param_grid)
+cv.fit(X_train,y_train)
+cv.best_estimator_
+#Making Predictions
+y_pred=cv.predict(X_test)
+#Model evaluation metrics
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+mean_absolute_error(y_test,y_pred)
+
+mean_squared_error(y_test,y_pred)**0.5
+
+r2_score(y_test,y_pred)
+
+#Reference for the metric results
+y.mean(),y.var(),y.std()
+
+
+
+
